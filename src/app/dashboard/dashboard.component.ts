59ea1b55987d0167984d92bccedb3ba5842da93d
pablo-parra <salfuve@gmail.com>
2017-07-26 20:24:35 +0200

    Deleting useless code

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,167 +1,72 @@
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
-
-import { Title }     from '@angular/platform-browser';
-
+import { Title } from '@angular/platform-browser';
 import { TdLoadingService, TdDigitsPipe } from '@covalent/core';
-
-import { UserService, IUser } from '../users';
-
-import { ItemsService, ProductsService, AlertsService } from '../../services';
-
-import { multi } from './data';
 
 @Component({
   selector: 'qs-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss'],
-  viewProviders: [ ItemsService, ProductsService, AlertsService ],
 })
 export class DashboardComponent implements OnInit {
-imageArray: any[] = [{
+  imageArray: any[] = [{
     columnSize: '40',
     column: [{
-        url: '../../assets/images/alex-lambley-205711.jpg',
-        isclicked: false,
-      }, {
-        url: '../../assets/images/norman-toth-177290.jpg',
-        isclicked: false,
-      }, {
-        url: '../../assets/images/kaci-baum-108756.jpg',
-        isclicked: false,
-      }, {
-        url: '../../assets/images/ariel-lustre-232891.jpg',
-        isclicked: false,
+      url: '../../assets/images/alex-lambley-205711.jpg',
+      isclicked: false,
+    }, {
+      url: '../../assets/images/norman-toth-177290.jpg',
+      isclicked: false,
+    }, {
+      url: '../../assets/images/kaci-baum-108756.jpg',
+      isclicked: false,
+    }, {
+      url: '../../assets/images/ariel-lustre-232891.jpg',
+      isclicked: false,
     }],
   }, {
     columnSize: '25',
     column: [{
-        url: '../../assets/images/pete-bellis-189599.jpg',
-        isclicked: false,
-      }, {
-        url: '../../assets/images/remy_loz-188297.jpg',
-        isclicked: false,
-      }, {
-        url: '../../assets/images/brooke-cagle-195860.jpg',
-        isclicked: false,
-      }, {
-        url: '../../assets/images/freestocks-org-195640.jpg',
-        isclicked: false,
-      }],
+      url: '../../assets/images/pete-bellis-189599.jpg',
+      isclicked: false,
+    }, {
+      url: '../../assets/images/remy_loz-188297.jpg',
+      isclicked: false,
+    }, {
+      url: '../../assets/images/brooke-cagle-195860.jpg',
+      isclicked: false,
+    }, {
+      url: '../../assets/images/freestocks-org-195640.jpg',
+      isclicked: false,
+    }],
   }, {
     columnSize: '25',
     column: [{
-        url: '../../assets/images/pete-bellis-189613.jpg',
-        isclicked: false,
-      }, {
-        url: '../../assets/images/pete-bellis-191833.jpg',
-        isclicked: false,
-      }],
+      url: '../../assets/images/pete-bellis-189613.jpg',
+      isclicked: false,
+    }, {
+      url: '../../assets/images/pete-bellis-191833.jpg',
+      isclicked: false,
+    },
+    ],
   }, {
     columnSize: '25',
     column: [{
-        url: '../../assets/images/valerie-elash-275588.jpg',
-        isclicked: false,
+      url: '../../assets/images/valerie-elash-275588.jpg',
+      isclicked: false,
+    },
+    {
+      url: '../../assets/images/jason-blackeye-221058.jpg',
+      isclicked: false,
     }],
   }];
-  items: Object[];
-  users: IUser[];
-  products: Object[];
-  alerts: Object[];
 
-  // Chart
-  single: any[];
-  multi: any[];
-
-  view: any[] = [700, 400];
-
-  // options
-  showXAxis: boolean = true;
-  showYAxis: boolean = true;
-  gradient: boolean = false;
-  showLegend: boolean = false;
-  showXAxisLabel: boolean = true;
-  xAxisLabel: string = '';
-  showYAxisLabel: boolean = true;
-  yAxisLabel: string = 'Sales';
-
-  colorScheme: any = {
-    domain: ['#1565C0', '#2196F3', '#81D4FA', '#FF9800', '#EF6C00'],
-  };
-
-  // line, area
-  autoScale: boolean = true;
-
-  constructor(private _titleService: Title,
-              private _itemsService: ItemsService,
-              private _userService: UserService,
-              private _alertsService: AlertsService,
-              private _productsService: ProductsService,
-              private _loadingService: TdLoadingService) {
-                // Chart
-                this.multi = multi.map((group: any) => {
-                  group.series = group.series.map((dataItem: any) => {
-                    dataItem.name = new Date(dataItem.name);
-                    return dataItem;
-                  });
-                  return group;
-                });
-  }
+  constructor(private _titleService: Title) { }
 
   ngOnInit(): void {
-    this._titleService.setTitle( 'Covalent Quickstart' );
-    this._loadingService.register('items.load');
-    this._itemsService.query().subscribe((items: Object[]) => {
-      this.items = items;
-      setTimeout(() => {
-        this._loadingService.resolve('items.load');
-      }, 750);
-    }, (error: Error) => {
-      this._itemsService.staticQuery().subscribe((items: Object[]) => {
-        this.items = items;
-        setTimeout(() => {
-          this._loadingService.resolve('items.load');
-        }, 750);
-      });
-    });
-    this._loadingService.register('alerts.load');
-    this._alertsService.query().subscribe((alerts: Object[]) => {
-      this.alerts = alerts;
-      setTimeout(() => {
-        this._loadingService.resolve('alerts.load');
-      }, 750);
-    });
-    this._loadingService.register('products.load');
-    this._productsService.query().subscribe((products: Object[]) => {
-      this.products = products;
-      setTimeout(() => {
-        this._loadingService.resolve('products.load');
-      }, 750);
-    });
-    this._loadingService.register('favorites.load');
-    this._productsService.query().subscribe((products: Object[]) => {
-      this.products = products;
-      setTimeout(() => {
-        this._loadingService.resolve('favorites.load');
-      }, 750);
-    });
-    this._loadingService.register('users.load');
-    this._userService.query().subscribe((users: IUser[]) => {
-      this.users = users;
-      setTimeout(() => {
-        this._loadingService.resolve('users.load');
-      }, 750);
-    }, (error: Error) => {
-      this._userService.staticQuery().subscribe((users: IUser[]) => {
-        this.users = users;
-        setTimeout(() => {
-          this._loadingService.resolve('users.load');
-        }, 750);
-      });
-    });
+    this._titleService.setTitle('Covalent Quickstart');
   }
+  // clickedImage(imageUrl: string): void {
 
-  // ngx transform using covalent digits pipe
-  axisDigits(val: any): any {
-    return new TdDigitsPipe().transform(val);
-  }
+  // }
 }
